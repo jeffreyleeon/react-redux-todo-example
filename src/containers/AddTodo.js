17486d95cf14b5646401e22460acc615b6b7ef0c
Lee Chun On <jeffreyleeon@gmail.com>
2017-06-06 11:11:30 +0800

    Create the presentational component of App.js

---
+++ src/containers/AddTodo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+  render() {
+    return (
+      <div>
+      	<p>Add Todo</p>
+      </div>
+    );
+  }
+}
+
+export default AddTodo;
